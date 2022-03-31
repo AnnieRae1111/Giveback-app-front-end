27fcff02e2486d4bbddc71b9f1b07696d9656082
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-03-31 13:35:16 -0600

    deploy

--- src/components/AvailableItemsCard.js
+++ src/components/AvailableItemsCard.js
@@ -1,141 +1,71 @@
-import { 
-    Col, 
-    CardColumns, 
-    Card, 
-    CardImg, 
-    CardBody, 
-    CardTitle, 
+ import {
+    Col,
+    CardColumns,
+    Card,
+    CardImg,
+    CardBody,
+    CardTitle,
     CardSubtitle,
     CardText,
-    Row, 
+    Row,
+    } from "reactstrap";
 
-}
-from 'reactstrap'
-import axios from 'axios'
+    import { Link } from "react-router-dom";
 
-import { Link } from 'react-router-dom'
-
-
-
-const AvailableItemsCard = ({claimItem, items, item, setItems, itemId,}) => {
-
-    // console.log(items, "all items array")
-   
-
-    // const [claimed, setClaimed]=useState([])
-    // const[itemHistory, setItemHistory]=useState([])
-   
-    // const[isClaimed, setIsClaimed]=useState(false)
-    // const[availableItems, setAvailableItems]=useState([])
+    const AvailableItemsCard = ({ deleteItem, claimItem, item }) => {
     
-    // // const [claimed, setClaimed]=useState([{
-    // //     category: item.category,
-    // //     title: item.title,
-    // //     photoUrl: item.photoUrl,
-    // //     description: item.description
-    // // }])
-
-    // useEffect(()=> {
-    //     // console.log(itemHistory, "item history in useEffect")
-
-    // },[itemHistory])
-    
-    // const[toggleEdit, setToggleEdit]=useState(false)
-    // const editItem = () => setToggleEdit(!toggleEdit)
-
-    // const claimItem = (id) => {
-    //     // let itemToClaim = items.filter((claimed)=> claimed._id === itemId)
-    //     let itemToClaim = items.find(eachItem => eachItem._id === id)
-    //     console.log(itemToClaim, "item that has been claimed")
-
-    //     // axios.post()
-    //     // setClaimed(itemToClaim)
-    //     setItemHistory(itemHistory=>[...itemHistory, itemToClaim])
-    //     // console.log(itemHistory, "item History array")
-
-    //     // let itemsNotClaimed = items.filter((item)=> item._id !== itemId)
-    //     // console.log(itemsNotClaimed, "items not claimed")
-    //     // setItems(itemsNotClaimed)
-    //     // console.log(availableItems, "remaining items")
-
-    // }
-
-    // console.log(claimed, "claimed items array")
-
-    
-
-    // const BASE_URL = "http://localhost:8000/api/items";
-
-    // const getAllItems = () => {
-    // axios.get(BASE_URL).then((res) => {
-    //     setAvailableItems(res.data);
-    //     console.log(availableItems);      
-    // });                            
-    // };
-
-    // useEffect(() => {
-    // getAllItems();
-    // }, []);
-
-   
-
-    const deleteUrl=`https://desolate-reaches-56728.herokuapp.com/api/items/${itemId}`
-        const deleteItem = async ( event ) => {
-            event.preventDefault()
-            await axios({
-                method:'DELETE',
-                url:deleteUrl,
-                data: null,
-            })
-            .then(res => console.log(res))
-            .then(window.location.reload(false))
-        }
-
-
-    
-    return (  
+    return (
         <div className="available-items-card-container">
-                 {/* <Row xs="1" m="2" l="2" xl="2" s="2"> */}
-                 <Row>
-            <Col > 
-        <CardColumns>
-            <Card className="available-items-card">
+        <Row>
+            <Col>
+            <CardColumns>
+                <Card className="available-items-card">
                 <CardImg
-                alt="Card image cap"
-                // src="https://i.imgur.com/TJMdSHN.jpg"
-                src={item.photoUrl}
-                top
-                width="100%"
+                    alt="Card image cap"
+                    src={item.photoUrl}
+                    top
+                    width="100%"
                 />
                 <CardBody>
-                <CardTitle tag="h5">
+                    <CardTitle tag="h5">
                     <strong>Category:</strong> {item.category}
-                </CardTitle>
-                <CardSubtitle
-                    className="mb-2 text-muted"
-                    tag="h6"
-                >
-                <strong>Title:</strong> {item.title}
-                </CardSubtitle>
-                <CardText>
+                    </CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    <strong>Title:</strong> {item.title}
+                    </CardSubtitle>
+                    <CardText>
                     <strong>Date Posted:</strong> {item.date_posted}
-                </CardText>
-                <CardText>
-                <strong>Posted By:</strong> {item.owner}
-                </CardText>
-                <CardText>
+                    </CardText>
+                    <CardText>
+                    <strong>Posted By:</strong> {item.owner}
+                    </CardText>
+                    <CardText>
                     <strong>Description:</strong> {item.description}
-                </CardText>
-                <button  className="claim-item-button" onClick={()=>claimItem(item._id)}>Claim item</button><br/>
-                <button className="claim-item-button" onClick={deleteItem}>Delete Post</button>
-                <Link to={`/edit/${item._id}`}><button className="claim-item-button">Edit</button></Link>
+                    </CardText>
+                    <button
+                    className="claim-item-button"
+                    onClick={() => claimItem(item._id)}
+                    >
+                    Claim item
+                    </button>
+                    <br />
+                    <button
+                    className="claim-item-button"
+                    onClick={() => deleteItem(item._id)}
+                    >
+                    Delete Post
+                    </button>
+                    <br />
+                    <Link to={`/edit/${item._id}`}>
+                    <button className="claim-item-button">Edit</button>
+                    </Link>
                 </CardBody>
-        </Card>
-        </CardColumns>
-        </Col>
+                </Card>
+            </CardColumns>
+            </Col>
         </Row>
         </div>
     );
-}
+    };
 
-export default AvailableItemsCard;
+    export default AvailableItemsCard;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,29 +1,125 @@
-    import '../css/LandingPage.css';
-    import { Bounce, Slide, Zoom } from 'react-awesome-reveal';
+import { useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 
-    const LandingPage = () => {
-    return (
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+
+const EditPost = ({
+items,
+handleEdit,
+editCategory,
+setEditCategory,
+editTitle,
+setEditTitle,
+editPhotoUrl,
+setEditPhotoUrl,
+editDescription,
+setEditDescription,
+}) => {
+const { id } = useParams();
+const item = items.find((item) => item._id === id);
+console.log(item, "this is the item");
+console.log(items, "all items in edit post");
+
+useEffect(() => {
+    if (item) {
+    setEditCategory(item.title);
+    setEditCategory(item.category);
+    setEditTitle(item.title);
+    setEditPhotoUrl(item.photoUrl);
+    }
+}, [
+    item,
+    setEditCategory,
+    setEditDescription,
+    setEditTitle,
+    setEditPhotoUrl,
+]);
+
+return (
+    <div className="upload-form-container">
+    {editTitle && (
         <>
-        <div className="landing-page-container">
-            <Bounce>
-            <h1 className="good-karma-landing-title">Good Karma</h1>
-            </Bounce>
-            <Zoom left>
-            <img
-                className="landing-page-image"
-                src="https://i.imgur.com/QpzScGC.jpg"
-                alt="landingpage"
+        <Form id="upload-form" onSubmit={(event) => event.preventDefault()}>
+            <FormGroup>
+            <Label for="categories">Category:</Label>
+            <Input
+                id="category"
+                name="category"
+                type="select"
+                value={editCategory}
+                onChange={(event) => {
+                setEditCategory(event.target.value);
+                }}
+            >
+                <option>Clothing</option>
+                <option>Books</option>
+                <option>Furniture</option>
+                <option>Household</option>
+                <option>Other</option>
+            </Input>
+            </FormGroup>
+            <FormGroup>
+            <Label id="title-label" for="exampleEmail">
+                Title:
+            </Label>
+            <Input
+                id="title"
+                name="title"
+                placeholder="title"
+                type="text"
+                value={editTitle}
+                onChange={(event) => {
+                setEditTitle(event.target.value);
+                }}
             />
-            </Zoom>
-            <Slide>
-            <h3 className="good-karma-tagline">
-                
-                Give your gently loved items to those in need...
-            </h3>
-            </Slide>
-        </div>
+            </FormGroup>
+            <FormGroup>
+            <Label id="description-label" for="description">
+                Description:
+            </Label>
+            <Input
+                id="description"
+                name="description"
+                placeholder="description"
+                type="text"
+                value={editDescription}
+                onChange={(event) => {
+                setEditDescription(event.target.value);
+                }}
+            />
+            </FormGroup>
+            <FormGroup>
+            <Label for="exampleFile">Photo URL</Label>
+            <Input
+                id="exampleFile"
+                name="url"
+                type="text"
+                value={editPhotoUrl}
+                onChange={(event) => {
+                setEditPhotoUrl(event.target.value);
+                }}
+            />
+            </FormGroup>
+            <Button
+            type="submit"
+            onClick={() => handleEdit(item._id)}
+            id="submit-button"
+            >
+            Submit
+            </Button>
+        </Form>
         </>
-    );
-    };
+    )}
+    {!editTitle && (
+        <>
+        <h2> Oops, no post here</h2>
+        <p>
+            <Link to="/"> Go back to Homepage</Link>
+        </p>
+        </>
+    )}
+    </div>
+);
+};
 
-    export default LandingPage;
+export default EditPost;
